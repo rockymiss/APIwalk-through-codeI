05b1516d9c417a30e1b58e0df04417b5ee17b6ce
rockymiss <rachelmcdermottrock@gmail.com>
2022-06-03 16:23:03 +0000

    Add key status to modal on check key

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,21 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
+}
+
+function displayStatus(data) {
+    // Needs to set heading text to API key status 
+    // body text to your key is valid until and date and show the modal 
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}<div>`
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
 }
